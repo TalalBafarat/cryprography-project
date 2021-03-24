4790ca2340170c9001861bb4dc49907a860e5db8
AbdulrahmanKaziha <80975353+AbdulrahmanKaziha@users.noreply.github.com>
2021-03-24 10:06:19 +0300

    h
    h

--- js/TripleDES.js
+++ js/TripleDES.js
@@ -1,4 +1,14 @@
-var encrypted = CryptoJS.TripleDES.encrypt("Hello123", "HowAreYou");
-console.log(encrypted.toString());
-var decrypted = CryptoJS.TripleDES.decrypt(encrypted, "HowAreYou");
-console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+var text = "hello123"
+var key = "1234567891011"
+var options = {mode: CryptoJS.mode.ECB};
+key = CryptoJS.enc.Utf16LE.parse(key);
+key = CryptoJS.MD5(key)
+key.words.push(key.words[0], key.words[1])
+var textWordArray = CryptoJS.enc.Utf16LE.parse(text);
+var encrypted = CryptoJS.TripleDES.encrypt(textWordArray, key, options);
+var base64String = encrypted.toString()
+console.log('Encrypted message: ' + base64String);
+var decrypted = CryptoJS.TripleDES.decrypt( {
+ciphertext: CryptoJS.enc.Base64.parse(encrypted.toString())}, key, options);
+console.log('Decrypted message: ' + decrypted.toString(CryptoJS.enc.Utf16LE));
