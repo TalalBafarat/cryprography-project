072d71b6ef6fcfd3560f09e014fd5dfd03f50cb2
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2021-04-06 14:11:30 +0300

    Update RSA.js

--- js/RSA.js
+++ js/RSA.js
@@ -1,14 +1,14 @@
 // The passphrase used to repeatably generate this RSA key.
 // The length of the RSA key, in bits.
 function encryptRSA(){
-    console.log("sss")
     var PassPhrase = document.getElementById("PassPhrase").value;
-    console.log(PassPhrase);
     var Bits = 1024;
-    console.log(Bits);
     var message = document.getElementById("mess").value;
+    console.log(message)    
+
     var ReciverRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
-    var ReciverPublicKeyString = cryptico.publicKeyString(ReciverRSAkey);       
+    console.log(ReciverRSAkey)
+    var ReciverPublicKeyString = cryptico.publicKeyString(ReciverRSAkey);   
     var EncryptionResult = cryptico.encrypt(message, ReciverPublicKeyString);
     var CipherText = EncryptionResult.cipher
     document.getElementById("encrypted").innerHTML=CipherText;
