fe1ae8860e4dbdb4f9e939ada101c84a064c3e88
Bassel Al-Droubi <71281647+basselDroubi@users.noreply.github.com>
2021-04-10 18:55:53 +0300

    d

--- js/AES.js
+++ js/AES.js
@@ -7,12 +7,28 @@
 //console.log(de.toString(CryptoJS.enc.Utf8));
 //console.log("hi");
 function encryptAES() {
-    var key = document.getElementById("password").value;
+   //var salty = CryptoJS.lib.WordArray.random(128/8);
+    //console.log(salty);
+    //var options = { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Iso97971};  
+    var d1 = new Date();
+        var n1 = d1.getTime();
     var message = document.getElementById("text").value;
-    var encrypted = CryptoJS.AES.encrypt(message, key);
-    //var ciphertext = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
-    document.getElementById("t").innerHTML = encrypted.toString();
-    console.log(encrypted.toString());
+    var key = document.getElementById("password").value;
+    if(message.length>0 && key.length>0){
+    
+      var encrypted = CryptoJS.AES.encrypt(message, key );
+      //var ciphertext = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
+      document.getElementById("t").innerHTML = encrypted;
+      console.log(encrypted.toString());
+      var d2 = new Date();
+        var n2 = d2.getTime();
+        var total = n2-n1;
+        document.getElementById("time").innerHTML=total+" miliseconds for 256 bits key ";
+    }else{
+      document.getElementById("t").innerHTML="there is no message or there is no PassPhrase ";
+    }
+  
+    
   }
   function decryptAES() {
     var key = document.getElementById("password").value.toString()
